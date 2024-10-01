df879a9fe64b49245de70bfaba3d338e408fbe0e
prince <153806877+princex29@users.noreply.github.com>
2024-10-01 14:50:35 +0530

    Add files via upload

---
+++ rate/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import *
+
+admin.site.register(Questions)
+admin.site.register(Options)
+admin.site.register(CustomerFeedback)
+admin.site.register(CustomerResponse)
+
+
+
+
+        
+
---
+++ rate/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, redirect
+from .models import *
+
+# Create your views here.
+
+
+def index(request):
+    feedbacks = CustomerFeedback.objects.all()
+    return render(request, 'survy.html', {"feedbacks": feedbacks})
+
+def customer_feedback(request, id):
+    feedback = CustomerFeedback.objects.get(id = id)
+    if request.method == "POST":
+        for question in feedback.question.all():
+            response_text = request.POST.get(f"response_{question.id}")
+            selected_option_ids = request.POST.getlist(f"options_{question.id}")
+
+            response = CustomerResponse.objects.create(
+                feedback = feedback,
+                question = question,
+                response_text = response_text if question.question_type in ["Text", "Bigtext"] else None
+            )
+            
+            if selected_option_ids:
+                selected_options = Options.objects.filter(id__in = selected_option_ids)
+                print(selected_options)
+                response.selected_options.set(selected_options)
+        return redirect("/thank-you/")    
+    
+    return render(request, 'survys.html', {"questions": feedback.question.all()})
+
+
+def thank_you(request):
+    return render(request, 'thank_you.html')
